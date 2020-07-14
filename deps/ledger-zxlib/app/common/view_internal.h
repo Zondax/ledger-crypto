4f9cec0dfa30ccb07df5c5a4830b11e1b7792328
Juan Leni <lenijuan@gmail.com>
2020-07-14 21:01:04 +0200

    reorganize ui mem usage

--- deps/ledger-zxlib/app/common/view_internal.h
+++ deps/ledger-zxlib/app/common/view_internal.h
@@ -27,8 +27,8 @@
 #define MAX_CHARS_PER_VALUE1_LINE   4096
 #define MAX_CHARS_HEXMESSAGE        160
 #else
-#define MAX_CHARS_PER_KEY_LINE      (2+1)
-#define MAX_CHARS_PER_VALUE_LINE    (4)
+#define MAX_CHARS_PER_KEY_LINE      (15+1)
+#define MAX_CHARS_PER_VALUE_LINE    (15)
 #define MAX_CHARS_PER_VALUE1_LINE   (2*MAX_CHARS_PER_VALUE_LINE+1)
 #define MAX_CHARS_PER_VALUE2_LINE   (MAX_CHARS_PER_VALUE_LINE+1)
 #define MAX_CHARS_HEXMESSAGE        40
@@ -42,14 +42,12 @@ typedef enum  {
 } review_mode_e;
 
 typedef struct {
-    union {
-        struct {
-            char key[MAX_CHARS_PER_KEY_LINE];
-            char value[MAX_CHARS_PER_VALUE1_LINE];
+    struct {
+        char key[MAX_CHARS_PER_KEY_LINE];
+        char value[MAX_CHARS_PER_VALUE1_LINE];
 #if defined(TARGET_NANOS)
-            char value2[MAX_CHARS_PER_VALUE2_LINE];
+        char value2[MAX_CHARS_PER_VALUE2_LINE];
 #endif
-        };
     };
     review_mode_e mode;
     uint8_t itemIdx;
